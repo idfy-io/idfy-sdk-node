d5f9353265bfacae7f102ce9bb6702e1a425d042
Christoffer Slettan <christofferslettan@gmail.com>
2018-12-04 14:49:07 +0100

    Implement query param helper

--- src/infrastructure/APIHelper.ts
+++ src/infrastructure/APIHelper.ts
@@ -1,5 +1,18 @@
 export default class APIHelper {
   public static appendQueryParams(url: string, params: { [key: string]: any }): string {
-    return url;
+    const q = [];
+    for (const p in params) {
+      if (params.hasOwnProperty(p) && params[p]) {
+        const val = params[p] instanceof Date
+          ? params[p].toISOString()
+          : params[p];
+
+        q.push(`${p}=${val}`);
+      }
+    }
+
+    return q.length > 0
+      ? `${url}?${q.join('&')}`
+      : url;
   }
 }
