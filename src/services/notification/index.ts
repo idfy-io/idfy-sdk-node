e450902ff645e0d68b0e777ba2d937868757df48
Christoffer Slettan <christofferslettan@gmail.com>
2018-12-07 08:54:46 +0100

    Update index.ts

--- src/services/notification/index.ts
+++ src/services/notification/index.ts
@@ -4,6 +4,7 @@ export * from './model/EventTypeInfo';
 export * from './model/MockEventRequest';
 export * from './model/Webhook';
 export * from './model/WebhookConfig';
+export * from './model/WebhookCreateConfig';
 export * from './model/WebhookResponse';
 export * from './model/WebhookUpdateOptions';
 export * from './model/WebhookDelivery';
