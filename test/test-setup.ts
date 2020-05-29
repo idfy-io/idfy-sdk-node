2a574c254644c2fbb852db45dff3fbca63b40dc3
Christoffer Slettan <christofferslettan@gmail.com>
2020-05-29 15:53:01 +0200

    Use Got in test-setup

--- test/test-setup.ts
+++ test/test-setup.ts
@@ -1,26 +1,19 @@
 /**
  * Global hook to make sure that the Idfy Mock Server is running
  */
-import * as request from 'request';
+import got from 'got';
 import { IdfyConfiguration } from '../src';
 
 IdfyConfiguration.setClientCredentials('idfy-test', 'foobar', []);
-IdfyConfiguration.baseUrl = 'http://localhost:5000';
+IdfyConfiguration.baseUrl = 'http://localhost:8111';
 
-before(function () {
-  this.timeout(5000);
+before(async function () {
+  this.timeout(6000);
+
   try {
-    return new Promise<any>((resolve, reject) => {
-      request(IdfyConfiguration.baseUrl, (err) => {
-        if (err) {
-          return reject(err);
-        }
-
-        console.log('✓ Idfy Mock Server running');
-        return resolve();
-      });
-    });
-  } catch {
+    await got(IdfyConfiguration.baseUrl, { throwHttpErrors: false, timeout: 5000 });
+    console.log('✓ Idfy Mock Server running');
+  } catch (err) {
     throw Error(`Failed to connect to Idfy Mock Server at ${IdfyConfiguration.baseUrl}, make sure it is running`);
   }
 });
