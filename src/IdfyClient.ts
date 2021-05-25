a2fd2e32b56092257c9cd39e2c24365da16dc31a
Christoffer Slettan <christofferslettan@gmail.com>
2021-05-25 19:04:52 +0200

    Add IDv2 to IdfyClient, tag IDv1 as deprecated

--- src/IdfyClient.ts
+++ src/IdfyClient.ts
@@ -1,10 +1,12 @@
 import { IdfyConfiguration } from './IdfyConfiguration';
 import { IdentificationService } from './services/identification';
+import { IdentificationV2Service } from './services/identificationv2';
 import { SignatureService } from './services/signature';
 import { NotificationService } from './services/notification';
 
 export class IdfyClient {
   private readonly identificationService: IdentificationService;
+  private readonly identificationV2Service: IdentificationV2Service;
   private readonly signatureService: SignatureService;
   private readonly notificationService: NotificationService;
 
@@ -12,12 +14,20 @@ export class IdfyClient {
     IdfyConfiguration.setClientCredentials(clientId, clientSecret, scopes);
 
     this.identificationService = new IdentificationService();
+    this.identificationV2Service = new IdentificationV2Service();
     this.signatureService = new SignatureService();
     this.notificationService = new NotificationService();
   }
 
+  /**
+   * @deprecated Use identificationV2 instead.
+   */
   public get identification(): IdentificationService {
     return this.identificationService;
+  }
+
+  public get identificationV2(): IdentificationV2Service {
+    return this.identificationV2Service;
   }
 
   public get signature(): SignatureService {
